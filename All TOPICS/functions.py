f08461c53fe4b89e9688195e807d1f0efe5781aa
Mahad Khan <mahadkhan122005@gmail.com>
2026-04-22 14:33:46 +0500

    add new function file

---
+++ All TOPICS/functions.py
@@ -0,0 +1,17 @@
+def greet():
+    print("hello buddy")
+greet()
+
+def greeting(name):
+    print("Hello " + name)
+greeting("jhons")
+#function with parameter and arguments and return value
+def add(a,b):
+    return a + b
+res = add(10,20)
+print(res)
+
+def power(num):
+    return num ** num
+result = power(5)
+print(result)
