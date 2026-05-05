596634a0897692c4add9e7deb9a6442a39ea9b41
Mahad Khan <mahadkhan122005@gmail.com>
2026-05-06 01:23:33 +0500

    add new file

---
+++ Pandas/Indexing_selection.py
@@ -0,0 +1,24 @@
+import pandas as pd
+data = {
+    "Name" : ["Alice","Bob",'Charlie',"David"],
+    "Age" : [20,25,35,55],
+    "City" : ['New York',"Paris","London","France"],
+    "Salary" : [20000,30000,50000,71000]
+}
+df = pd.DataFrame(data)
+print(df)
+print("**************")
+#with loc selection in loc selection last Index is include not exclude
+subset_loc = df.loc[0:3,['Name','Age','Salary']]
+print(subset_loc)
+print("**************")
+#with iloc selection in iloc selection last index is exclude not include
+subset_iloc = df.iloc[0:3,1:3]
+print(subset_iloc)
+print("**************")
+#boolean filtering
+high_salary = df[df['Salary'] > 30000]
+print(high_salary)
+
+df.loc[df['Age'] < 30 , ['Salary']] = 65000
+print("Low Age Salary Change\n", df.loc[df['Age'] <30 ,['Salary']])
